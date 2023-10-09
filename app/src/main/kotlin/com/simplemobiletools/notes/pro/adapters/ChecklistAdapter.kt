SimpleMobileTools/Simple-Notes

Remove status icon from checklist items. Always show drag handle on checklist items.

app/src/main/kotlin/com/simplemobiletools/notes/pro/adapters/ChecklistAdapter.kt
@@ -33,8 +33,6 @@ class ChecklistAdapter(activity: BaseSimpleActivity, var items: ArrayList<Checkl
                        recyclerView: MyRecyclerView, val showIcons: Boolean, itemClick: (Any) -> Unit) :
         MyRecyclerViewAdapter(activity, recyclerView, null, itemClick), ItemTouchHelperContract {
 
-    private lateinit var crossDrawable: Drawable
-    private lateinit var checkDrawable: Drawable
     private var touchHelper: ItemTouchHelper? = null
     private var startReorderDragListener: StartReorderDragListener
 
@@ -106,8 +104,6 @@ class ChecklistAdapter(activity: BaseSimpleActivity, var items: ArrayList<Checkl
 
     private fun initDrawables() {
         val res = activity.resources
-        crossDrawable = res.getColoredDrawableWithColor(R.drawable.ic_cross_vector, res.getColor(R.color.md_red_700))
-        checkDrawable = res.getColoredDrawableWithColor(R.drawable.ic_check_vector, res.getColor(R.color.md_green_700))
     }
 
     private fun renameChecklistItem() {
@@ -193,11 +189,9 @@ class ChecklistAdapter(activity: BaseSimpleActivity, var items: ArrayList<Checkl
                 }
             }
 
-            checklist_image.setImageDrawable(if (checklistItem.isDone) checkDrawable else crossDrawable)
-            checklist_image.beVisibleIf(showIcons)
             checklist_holder.isSelected = isSelected
 
-            checklist_drag_handle.beVisibleIf(selectedKeys.isNotEmpty())
+            checklist_drag_handle.beVisibleIf(showIcons)
             checklist_drag_handle.applyColorFilter(textColor)
             checklist_drag_handle.setOnTouchListener { v, event ->
                 if (event.action == MotionEvent.ACTION_DOWN) {

fabian-thomas/Simple-Notes-Simple-Drag

app/src/main/kotlin/com/simplemobiletools/notes/pro/adapters/ChecklistAdapter.kt
@@ -36,8 +36,6 @@ class ChecklistAdapter(
 ) :
     MyRecyclerViewAdapter(activity, recyclerView, itemClick), ItemTouchHelperContract {
 
-    private lateinit var crossDrawable: Drawable
-    private lateinit var checkDrawable: Drawable
     private var touchHelper: ItemTouchHelper? = null
     private var startReorderDragListener: StartReorderDragListener
 
@@ -111,14 +109,6 @@ class ChecklistAdapter(
 
     private fun initDrawables() {
         val res = activity.resources
-        crossDrawable = res.getColoredDrawableWithColor(
-            com.simplemobiletools.commons.R.drawable.ic_cross_vector,
-            res.getColor(com.simplemobiletools.commons.R.color.md_red_700)
-        )
-        checkDrawable = res.getColoredDrawableWithColor(
-            com.simplemobiletools.commons.R.drawable.ic_check_vector,
-            res.getColor(com.simplemobiletools.commons.R.color.md_green_700)
-        )
     }
 
     private fun renameChecklistItem() {
@@ -206,11 +196,9 @@ class ChecklistAdapter(
                 }
             }
 
-            checklistImage.setImageDrawable(if (checklistItem.isDone) checkDrawable else crossDrawable)
-            checklistImage.beVisibleIf(showIcons && selectedKeys.isEmpty())
             checklistHolder.isSelected = isSelected
 
-            checklistDragHandle.beVisibleIf(selectedKeys.isNotEmpty())
+            checklistDragHandle.beVisibleIf(showIcons)
             checklistDragHandle.applyColorFilter(textColor)
             checklistDragHandle.setOnTouchListener { v, event ->
                 if (event.action == MotionEvent.ACTION_DOWN) {